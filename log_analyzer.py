196c58741f6384bde800966c81242d055f00f57d
Ahad <ahaddilanov@gmail.com>
2026-08-27 16:13:20 +0400

    Add brute-force login detection

--- log_analyzer.py
+++ log_analyzer.py
@@ -3,6 +3,21 @@ def read_log(filepath):
         lines = f.readlines()
     return lines
 
+def detect_failed_logins(log_lines, threshold=5):
+    failed_attempts = {}
+
+    for line in log_lines:
+        if "401" in line: #401 indicates that this is unauthorized
+            ip = line.split(" ")[0]
+            failed_attempts[ip] = failed_attempts.get(ip, 0) + 1
+
+    suspicious_ips = {}
+    for ip in failed_attempts:
+        if failed_attempts[ip] >= threshold:
+            suspicious_ips[ip] = failed_attempts[ip]
+
+    return suspicious_ips
+
 log_lines = read_log("sample_log.txt")
-print(f"Total lines: {len(log_lines)}")
-print(log_lines[0])
+suspicious = detect_failed_logins(log_lines)
+print("Suspicious IPs (brute-force):", suspicious)
